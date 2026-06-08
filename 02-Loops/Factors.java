7e995d2e39ffc43457f13cac2633268161142c6f
vaikunthprajapati <vaikunthprajapati775@gmail.com>
2026-06-08 15:53:40 +0530

    Added Factors Problem

---
+++ 02-Loops/Factors.java
@@ -0,0 +1,80 @@
+// Print all factors of a number (loop up to number)
+public class Factors {
+    public static void main(String[] args) {
+        int number = 120;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+// Print all factors optimized: loop up to number/2, then print number itself
+class Factors2 {
+    public static void main(String[] args) {
+        int number = 120;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print(number + " ");
+    }
+}
+
+// Count total factors of a number
+class Factors3 {
+    public static void main(String[] args) {
+        int number = 120;
+        int count = 1;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        System.out.print("Count is " + count);
+    }
+}
+
+// Count only even factors of a number
+class Factors4 {
+    public static void main(String[] args) {
+        int number = 111;
+        int count;
+        if (number % 2 == 0) {
+            count = 1;
+        } else {
+            count = 0;
+        }
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                if (i % 2 == 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.print("Count is " + count);
+    }
+}
+
+// Difference between sum of odd factors and sum of even factors
+class Factors5 {
+    public static void main(String[] args) {
+        int number = 120;
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                if (i % 2 == 0) {
+                    evenSum += i;
+                } else {
+                    oddSum += i;
+                }
+            }
+        }
+        int difference = oddSum - evenSum;
+        System.out.println("The Difference is :" + difference);
+    }
+}
