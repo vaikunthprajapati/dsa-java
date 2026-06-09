be9db7dcd28f3e7c288e9c750739931d8d07bf30
vaikunthprajapati <vaikunthprajapati775@gmail.com>
2026-06-09 16:16:32 +0530

    Added For loop basics Problem

---
+++ 02-Loops/ForLoopBasics.java
@@ -0,0 +1,17 @@
+// Print 1 to 100 using for loop
+public class ForLoopBasics {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+// Print from 55 down to 23 (56 excluded, 23 included)
+class ForLoopBasics2 {
+    public static void main(String[] args) {
+        for (int i = 55; i >= 23; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
